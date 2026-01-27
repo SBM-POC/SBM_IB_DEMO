bc82943ccb9a462708dfc8f641d489b3be6e285c
User <User@RQC-LAPTOP-009>
2026-01-27 17:29:07 +0400

    Delete Files

--- tests/PdfReader.test.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "@playwright/test";
-import { readPdfData } from "../Helpers/RecieptHandler";
-
-test("should read PDF and verify 'Transaction Successful'", async () => {
-  const text = await readPdfData("TestPDF.pdf");
-
-  console.log("📝 Extracted PDF text:\n", text);
-});
